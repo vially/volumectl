c55c55f217ead29f363ac171a7af2df3f8022bd3
Valentin Hăloiu <vially.ichb@gmail.com>
2019-09-15 18:36:38 +0200

    Reuse existing notification

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vially/volumectl
 go 1.13
 
 require (
+	github.com/adrg/xdg v0.0.0-20190319220657-88e5137d2444
 	github.com/esiqveland/notify v0.1.2
 	github.com/godbus/dbus v5.0.1+incompatible
 	github.com/urfave/cli v1.22.1
--- notify.go
+++ notify.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/adrg/xdg"
 	"github.com/esiqveland/notify"
 	"github.com/godbus/dbus"
 	"golang.org/x/xerrors"
 )
 
-var defaultNotificationClient = &notificationClient{}
+var defaultNotificationClient = &notificationClient{
+	store: &notificationStoreClient{},
+}
 
-type notificationClient struct{}
+type notificationClient struct {
+	store notificationStore
+}
 
 func (c *notificationClient) showVolumeNotification(volume int, mute bool) error {
 	conn, err := dbus.SessionBus()
@@ -19,6 +31,7 @@ func (c *notificationClient) showVolumeNotification(volume int, mute bool) error
 	n := notify.Notification{
 		AppName:       "volumectl",
 		AppIcon:       c.notificationVolumeIcon(volume, mute),
+		ReplacesID:    c.store.LastNoficationID(),
 		ExpireTimeout: int32(3000),
 		Hints: map[string]dbus.Variant{
 			"value":       dbus.MakeVariant(volume),
@@ -26,10 +39,11 @@ func (c *notificationClient) showVolumeNotification(volume int, mute bool) error
 		},
 	}
 
-	if _, err := notify.SendNotification(conn, n); err != nil {
+	notificationID, err := notify.SendNotification(conn, n)
+	if err != nil {
 		return xerrors.Errorf("error sending notification: %w", err)
 	}
-	return nil
+	return c.store.WriteNotificationID(notificationID)
 }
 
 func (c *notificationClient) notificationVolumeIcon(volume int, mute bool) string {
@@ -48,4 +62,57 @@ func (c *notificationClient) notificationVolumeIcon(volume int, mute bool) strin
 
 func showVolumeNotification(volume int, mute bool) error {
 	return defaultNotificationClient.showVolumeNotification(volume, mute)
+}
+
+type notificationStore interface {
+	LastNoficationID() uint32
+	WriteNotificationID(uint32) error
+}
+
+type notificationStoreClient struct{}
+
+var _ notificationStore = &notificationStoreClient{}
+
+func (s *notificationStoreClient) LastNoficationID() uint32 {
+	storeFilePath, err := xdg.SearchRuntimeFile(s.storeFilePath())
+	if err != nil {
+		return 0
+	}
+
+	storeFileContent, err := ioutil.ReadFile(storeFilePath)
+	if err != nil {
+		return 0
+	}
+
+	lastNotificationID := strings.TrimSpace(string(storeFileContent))
+	notificationID, _ := strconv.ParseUint(lastNotificationID, 10, 32)
+	return uint32(notificationID)
+}
+
+func (s *notificationStoreClient) WriteNotificationID(id uint32) error {
+	if err := ensureDirExists(path.Join(xdg.RuntimeDir, "volumectl")); err != nil {
+		return xerrors.Errorf("unable to create XDG runtime directory: %w", err)
+	}
+
+	storeFilePath := path.Join(xdg.RuntimeDir, s.storeFilePath())
+	return ioutil.WriteFile(storeFilePath, []byte(fmt.Sprintf("%d", id)), 0600)
+}
+
+func (s *notificationStoreClient) storeFilePath() string {
+	return path.Join("volumectl", "last_notification_id")
+}
+
+func ensureDirExists(dir string) error {
+	fi, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return os.Mkdir(dir, os.ModeDir|0700)
+		}
+		return err
+	}
+
+	if !fi.IsDir() {
+		return xerrors.Errorf("%d is not a directory", dir)
+	}
+	return nil
 }
